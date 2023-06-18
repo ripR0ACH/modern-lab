a8b4da501c403b6fc207d75ca9a45bad340dfe74
zach <morrisonizach@gmail.com>
2023-06-18 17:06:17 -0500

    trying to get my blackbody curve to fit

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -3,21 +3,18 @@ import numpy as np
 from scipy.optimize import curve_fit
 from decimal import *
 
-data = ["data061323_highbw_lowsens.txt", "data061323_lowbw_highsens.txt"]
+data = ["data061523_highbw_lowsens.txt", "data061523_lowbw_highsens.txt"]
 calibration_data = ["calibration1.txt", "calibration2.txt"]
 wavelength_calibration = "wavelength_calibration.txt"
 sens_cal = "sens_calibration.txt"
 
-h = 6.626e-34
-c = 2.998e8
-k = 1.381e-23
-T_guess = 3000
+T_guess = 5777
 
-def fit_func(pix, i, c1, c2, c3):
-    return i + c1 * pix + c2 * pix**2 + c3 * pix**3
-a = 2 * h * np.power(c, 2)
-def blackbody(w, T):
-    return (a / np.power(w, 5)) * (1 / np.expm1((h * c) / (k * T * w)))
+def blackbody(w, T, c1):
+    from scipy.constants import h, k, c
+    a = 2 * h * np.power(c, 2)
+    b = h * c / (w * k * T)
+    return c1 * a / (np.power(w, 5) * np.expm1(b))
 class Run:
     def __init__(self, title):
         self.title = title
@@ -30,14 +27,6 @@ class Run:
     def add_data(self, w, i):
         self.__add_wavelength(w)
         self.__add_intensity(i)
-with open(wavelength_calibration, "r") as f:
-    lines = f.readlines()
-    wave = Run(lines[0].replace('\n', ''))
-    for line in lines:
-        if len(line.split()) != 2:
-            continue
-        else:
-            wave.add_data(line.split()[0], line.split()[1])
 with open(sens_cal, "r") as f:
     lines = f.readlines()
     sens_factor = Run("Sensitivity Factor Calibration")
@@ -49,7 +38,7 @@ vals_diff = []
 for i in range(len(expected_vals)-1):
     x = true_vals[i] - expected_vals[i]
     vals_diff.append(x)
-err = np.mean(vals_diff)
+err = np.mean(vals_diff) * 1e-9
 sens_i = 0
 sensors = []
 for d in data:
@@ -64,7 +53,6 @@ for d in data:
             else:
                 runs[i].add_data(*line.split())
     sensors.append(runs)
-fig, ax = plt.subplots(2, 1)
 for sensor in range(len(sensors)):
     for j in range(len(sensors[sensor])):
         for k in range(len(sensors[sensor][j].w)):
@@ -76,10 +64,14 @@ for sensor in range(len(sensors)):
                 while sensors[sensor][j].w[k] > sens_factor.w[sens_i]:
                     sens_i += 1
                 sensors[sensor][j].i[k] *= sens_factor.i[sens_i] / 100
+        sens_i = 0
+fig, ax = plt.subplots(2, 1)
 for s in sensors[0]:
-    ax[0].plot(s.w, s.i, label = s.title)
-ax[0].plot(wave.w, blackbody(wave.w, T_guess))
+    ax[0].plot(np.array(s.w) * 1e9, s.i, label = s.title)
+for s in sensors[1]:
+    ax[1].plot(np.array(s.w) * 1e9, s.i, label = s.title)
+T_guess = 0.0029 / sensors[0][0].w[sensors[0][0].i.index(max(sensors[0][0].i))]
+popt, pcov = curve_fit(blackbody, sensors[0][0].w, sensors[0][0].i, p0 = [T_guess, 0], maxfev = 5000)
+print(popt[0], popt[1])
+ax[0].plot(np.linspace(1e-9, 1e-6, 1000) * 1e9, blackbody(np.linspace(1e-9, 1e-6, 1000), popt[0], popt[1]), c="k")
 plt.show()
-# for s in sensors[1]:
-#     ax[1].plot(s.w, s.i, label = s.title)
-# plt.show()
