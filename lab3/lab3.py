ffa48c7c6896d69d27111d699c07e2c90fb31f8a
zach <morrisonizach@gmail.com>
2023-06-25 15:06:58 -0500

    pushing code for the third lab

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-element_files = ["Cd109.Txt", "Co57.Txt", "Co60.Txt", "Cs137.Txt", "Mn54.Txt", "Na22.Txt", "Zn65.Txt"]
+element_files = ["Cd109.Txt", "Co57.Txt", "Co60.Txt", "Cs137.Txt", "K40.Txt","Mn54.Txt", "Na22.Txt", "Zn65.Txt"]
 data = ["data/" + i for i in element_files]
 
 E_CONST = 1.972973
@@ -25,13 +25,13 @@ class Element():
         e = np.array([])
         with open(file, "r") as f:
             lines = f.readlines()
-            d_start = 0
-            for l in range(len(lines)):
-                if lines[l] == "SPECTRUM\n":
-                    d_start = l + 1
-                    break
-            for l in lines[d_start:]:
-                e = np.append(e, int(l.split()[0][:-1]) * E_CONST)
+        d_start = 0
+        for l in range(len(lines)):
+            if lines[l] == "SPECTRUM\n":
+                d_start = l + 1
+                break
+        for l in lines[d_start:]:
+            e = np.append(e, float(l.split()[0][:-1]) * E_CONST)
         return e
     def get_energy(self) -> np.array(list()):
         return self.__e
@@ -62,13 +62,32 @@ class Element():
         return self.__avg_counts
     def __str__(self) -> str:
         return self.get_name()
-elements = np.array([])
-for d in data:
-    elements = np.append(elements, Element(d))
-for e in elements:
-    fig, ax = plt.subplots(1, 1)
-    ax.plot(e.get_energy(), e.get_avg_counts())
-    ax.set_xlim([0, 1.5e3])
-    ax.set_title(e.get_name())
-    fig.show()
-plt.show()
+def set_elements(d, e = np.array([])) -> np.array([]):
+    for i in d:
+        e = np.append(e, Element(i))
+    return e
+def gaussian(x, height, center, stdev) -> float:
+    return height * np.exp(-1 * np.power(x - center, 2) / (2 * stdev ** 2))
+def plot_elements(e, fit) -> None:
+    for i in e:
+        fig, ax = plt.subplots(1, 1)
+        ax.plot(i.get_energy(), i.get_avg_counts())
+        ax.set_xlim([0.55e3, 0.75e3])
+        ax.set_ylim([0, 500])
+        ax.set_title(i.get_name())
+        ax.plot(np.linspace(500, 1000, 1000), gaussian(np.linspace(500, 1000, 1000), *fit), c = "k")
+        fig.show()
+    plt.show()
+    return
+def fit_gaussian(x, y) -> tuple[list, list]:
+    from scipy.optimize import curve_fit
+    return curve_fit(gaussian, x, y, p0=[400, 665, 1])
+def main():
+    elements = set_elements(data)
+    popt, pcov = fit_gaussian(elements[0].get_energy()[30:100], elements[0].get_avg_counts()[30:100])
+    print(max(elements[0].get_avg_counts()[30:100]))
+    print(popt, "\n", pcov)
+    plot_elements(elements[:1], popt)
+if __name__ == "__main__":
+    main()
+# 2nd derivative to find concavity
